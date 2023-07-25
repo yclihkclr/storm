db38e0ec283ecc825e2b5c02104f00e0c8a225cf
yclihkclr <ycli@hkclr.hk>
2023-07-25 21:36:10 +0800

    replace cylinder primitive with box

--- examples/load_problems.py
+++ examples/load_problems.py
@@ -19,13 +19,6 @@ def get_world_param_from_problemset(problem_chosen):
                         'dims': [2.0, 2.0, 0.2],
                         'pose': [0.0, 0.0, -0.1, 0, 0, 0, 1.0]
                     }
-                },
-                'cylinder': {
-                    'cylinder1': {
-                        'radius': 0.001,
-                        'pose': [10.2, -0.0, 0.4, 0, 0, 0, 1.0],
-                        'height': 0.001
-                    }
                 }
             }
         }
@@ -43,17 +36,71 @@ def get_world_param_from_problemset(problem_chosen):
         if isinstance(obs, Cuboid):
             cub_key = 'cube' + str(cub_idx)
             cub_idx += 1
-            obs_pose = obs.center + [0, 0, 0, 1.0]
+            obs_pose = obs.center + [obs.pose.so3._quat.x,obs.pose.so3._quat.y,obs.pose.so3._quat.z,obs.pose.so3._quat.w]
             world_params['world_model']['coll_objs']['cube'][cub_key] = {'dims': obs.dims,
                                                                          'pose': obs_pose}
         if isinstance(obs, Cylinder):
+            # replace cylinder with cube
             cyl_key = 'cylinder' + str(cyl_idx)
             cyl_idx += 1
-            obs_pose = obs.center + [0, 0, 0, 1.0]
-            world_params['world_model']['coll_objs']['cylinder'][cyl_key] = {'radius': obs.radius,
-                                                                             'pose': obs_pose,
-                                                                           'height': obs.height}
+            obs_pose = obs.center + [obs.pose.so3._quat.x,obs.pose.so3._quat.y,obs.pose.so3._quat.z,obs.pose.so3._quat.w]
+            obs_dims = [2*obs.radius,2*obs.radius,obs.height]
+            world_params['world_model']['coll_objs']['cube'][cyl_key] = {'dims': obs_dims,
+                                                                         'pose': obs_pose}
     return world_params
+
+# dont consider the cylinder due to lack of gym api
+# def get_world_param_from_problemset(problem_chosen):
+#     #init nested dictionary
+#     world_params = {
+#         'world_model': {
+#             'coll_objs': {
+#                 'sphere': {
+#                     'sphere1': {
+#                         'radius': 0.001,
+#                         'position': [10.2, -0.0, 0.4]
+#                     }
+#                 },
+#                 'cube': {
+#                     'cube1': {
+#                         'dims': [2.0, 2.0, 0.2],
+#                         'pose': [0.0, 0.0, -0.1, 0, 0, 0, 1.0]
+#                     }
+#                 },
+#                 'cylinder': {
+#                     'cylinder1': {
+#                         'radius': 0.001,
+#                         'pose': [10.2, -0.0, 0.4, 0, 0, 0, 1.0],
+#                         'height': 0.001
+#                     }
+#                 }
+#             }
+#         }
+#     }
+#     # enumerate from problem_chosen.obstacles
+#     sph_idx = 1
+#     cub_idx = 1
+#     cyl_idx = 1
+#     for idx, obs in enumerate(problem_chosen.obstacles):
+#         if isinstance(obs, Sphere):
+#             sph_key = 'sphere' + str(sph_idx)
+#             sph_idx += 1
+#             world_params['world_model']['coll_objs']['sphere'][sph_key] = {'radius': obs.radius,
+#                                                                            'position': obs.center}
+#         if isinstance(obs, Cuboid):
+#             cub_key = 'cube' + str(cub_idx)
+#             cub_idx += 1
+#             obs_pose = obs.center + [obs.pose.so3._quat.x,obs.pose.so3._quat.y,obs.pose.so3._quat.z,obs.pose.so3._quat.w]
+#             world_params['world_model']['coll_objs']['cube'][cub_key] = {'dims': obs.dims,
+#                                                                          'pose': obs_pose}
+#         if isinstance(obs, Cylinder):
+#             cyl_key = 'cylinder' + str(cyl_idx)
+#             cyl_idx += 1
+#             obs_pose = obs.center + [obs.pose.so3._quat.x,obs.pose.so3._quat.y,obs.pose.so3._quat.z,obs.pose.so3._quat.w]
+#             world_params['world_model']['coll_objs']['cylinder'][cyl_key] = {'radius': obs.radius,
+#                                                                              'pose': obs_pose,
+#                                                                            'height': obs.height}
+#     return world_params
 
 if __name__ == "__main__":
     # parser = argparse.ArgumentParser()
@@ -76,7 +123,6 @@ if __name__ == "__main__":
     # args = parser.parse_args()
     # env_type = args.environment_type.replace("-", "_")
     # problem_type = args.problem_type.replace("-", "_")
-
     file_path = "/home/andylee/storm/mpinets/hybrid_solvable_problems.pkl"
     with open(file_path, "rb") as f:
         problems = pickle.load(f)
